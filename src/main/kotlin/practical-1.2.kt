3cbb8ae5e90196e3c30cb6076d5040e03f546bba
student <student@gnu.ac.in>
2023-07-14 08:47:54 +0530

    Created practical 1.1

---
+++ src/main/kotlin/practical-1.2.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println("Hello word!")
+    var a: Int = 10
+    var b: String = "Hello sagar"
+    var d: Double = 10.5
+    var f: Float = 50.60f
+    var l: Long = 13435212
+    var s: Short = -2
+    var c: Char = 'F'
+    var x: Boolean = false
+    var y: Byte = 127
+
+
+
+}
