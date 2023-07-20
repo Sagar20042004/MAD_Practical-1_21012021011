a85fbe93ad5d3e5d377c9c89b7b0e419b0cdd8be
Lenovo <sagarbheda2004@gmail.com>
2023-07-20 22:45:33 +0530

    practical-1.7.kt

--- src/main/kotlin/practical-1.7.kt
+++ src/main/kotlin/practical-1.7.kt
@@ -3,15 +3,18 @@ import java.util.Scanner
 fun main(){
     var reader = Scanner(System.`in`)
 
+    println("Bheda sagar / 21012021011")
     println("Enter the number : ")
-    var num = reader.nextLong()
+    var num = reader.nextInt()
 
     var x = fect(num)
     println("factorial is :$x")
 
 }
 
-fun fect(num1 :Long):Long{
-    var temp = 0
-    return temp * fect(num1-1)
+tailrec fun fect(num1 :Int , ans:Int=1):Long{
+
+    return if (num1==1) ans.toLong()
+    else fect(num1-1 , ans * num1)
+
 }
